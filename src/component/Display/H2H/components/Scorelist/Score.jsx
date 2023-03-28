41e85f860ef7d1718278930aefd36de2f0c4cbe2
nirmalroyalrich <nirmalroy4@gmail.com>
2023-03-28 22:12:42 +0530

    ScoreBoardH2H

--- src/component/Display/H2H/components/Scorelist/Score.jsx
+++ src/component/Display/H2H/components/Scorelist/Score.jsx
@@ -4,12 +4,18 @@ import React from 'react'
 
 
 
-export  function Score(scoreLeft, topic, scoreRight) {
+export function Score({scoreLeft, topic, scoreRight}) {
   return (
     <div className='score-wrapper'>
-      <div className="score-left">{scoreLeft}</div>
-      <div className="topic">{topic}</div>
-      <div className="score-right">{scoreRight}</div>
+      <div className="score-left">
+        <h2 className="text-50-white">{scoreLeft}</h2>
+      </div>
+      <div className="topic">
+        <h1 className="text-50-white">{topic}</h1>
+      </div>
+      <div className="score-right">
+        <h2 className="text-50-white">{scoreRight}</h2>
+      </div>
     </div>
   )
 }
--- src/component/Display/H2H/container/main/h2h.jsx
+++ src/component/Display/H2H/container/main/h2h.jsx
@@ -9,17 +9,52 @@ const scoreData =[
         scoreLeft: "0",
         topic: "Elimination",
         scoreRight: "0"
+    },
+    {
+        scoreLeft: "0",
+        topic: "Match Played",
+        scoreRight: "0"
+    },
+    {
+        scoreLeft: "0",
+        topic: "Contribution",
+        scoreRight: "0"
+    },
+    {
+        scoreLeft: "0",
+        topic: "AVG Elimination",
+        scoreRight: "0"
+    },
+    {
+        scoreLeft: "0",
+        topic: "Overall Kills",
+        scoreRight: "0"
+    },
+    {
+        scoreLeft: "0",
+        topic: "Ranking",
+        scoreRight: "0"
     }
 ]
 
 export default function H2h() {
   return (
-    <div className="score-wrapper">
-    {scoreData.map((scoreLeft, topic, scoreRight) => (
-        <Score scoreLeft={scoreLeft} topic={topic} scoreRight={scoreRight} />
-    ))}
-    <h2>this is </h2>
-</div>
+    <div className="main">
+        <section className="main-content">
+            <div className="header">
+                <p className='text-50-white'>Player Name</p>
+                <div className="vs-component">
+                    <p className='text-50-white'>VS</p>
+                </div>
+                <p className='text-50-white'>Player Name</p>
+            </div>
+            <div className='scores'>
+            {scoreData.map((item) => (
+                <Score scoreLeft={item.scoreLeft} topic={item.topic} scoreRight={item.scoreRight} />
+            ))}
+            </div>
+        </section>
+    </div>
   )
 }
 
